ab631b5069cc0e232d537f19194038bcf3351e99
Jeffers199817 <jefferson.alquinga1998@gmail.com>
2025-05-18 18:25:21 -0500

    Integración de impresion de variables

--- 03-counter-app-vite/src/FirstApp.jsx
+++ 03-counter-app-vite/src/FirstApp.jsx
@@ -1,4 +1,36 @@
-export const FirstApp = ()=>{
+import { useState } from "react";
 
-    return <h1>El FirstApp</h1>
-}
+export const FirstApp = () => {
+  const estudiante = {
+    nombre: "jefferson",
+    apellido: "Alquinga",
+    edad: 27,
+    direccion: "Puembo-chiche",
+    lugar: "Quito",
+  };
+
+  const [esGraduado, setEsGraduado] = useState(null);
+
+  const estudianteFunction = (graduado) =>
+    graduado === true ? "Sí está graduado" : "No está graduado";
+
+  const handleGraduado = (respuesta) => {
+    setEsGraduado(respuesta === "SÍ");
+  };
+
+  return (
+    <>
+      <h1>{estudiante.nombre}</h1>
+      <code>{JSON.stringify(estudiante, null, 2)}</code>
+      <div>
+        <p>
+          ¿{estudiante.nombre} {estudiante.apellido} es graduado?
+        </p>
+        <button onClick={() => handleGraduado("SÍ")}>SÍ</button>
+        <button onClick={() => handleGraduado("NO")}>NO</button>
+      </div>
+      {esGraduado !== null && <h1>{estudianteFunction(esGraduado)}</h1>}
+      <p>Estudiante de Ingeniería</p>
+    </>
+  );
+};
