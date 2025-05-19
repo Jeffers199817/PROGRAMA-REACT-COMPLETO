c21126e92ca31761c550ce1cecab037988e6f8f8
Jeffers199817 <jefferson.alquinga1998@gmail.com>
2025-05-19 17:47:17 -0500

    Integración de los hooks

--- 03-counter-app-vite/src/CounterApp.jsx
+++ 03-counter-app-vite/src/CounterApp.jsx
@@ -1,14 +1,30 @@
 
 import PropTypes from "prop-types"
+import { useState } from "react";
+
 
 
 export const CounterApp = ({ value = 4 }) => {
-    
+
+    const [counter, setCounter] = useState(value);
+
+    //FUNCIONES
+    //AGREGAR
+    const handleAdd = () => { setCounter((c) => c + 1); };
+    //SUBTRACT
+    const handleSubtract = () => { setCounter((c) => c - 1) };
+    //RESET
+    const handleReset = () => { setCounter((c) => (c*0 ))}
+
+
 
     return <>
     
         <h1>CounterAPP</h1>
-        <h2>El valor es: {value}</h2>
+        <h2>El valor es: {counter}</h2>
+        <button onClick={handleAdd}> +1</button >
+        <button onClick={handleSubtract}> --1</button>
+        <button onClick={handleReset}>Reset</button>
     
     </>
 
