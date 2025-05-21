121a8c32ae7f3bd239c640b9167201298f00b233
Jeffers199817 <jefferson.alquinga1998@gmail.com>
2025-05-21 16:41:23 -0500

    Integración de los test para la platafomra de react

--- 03-counter-app-vite/src/FirstApp.jsx
+++ 03-counter-app-vite/src/FirstApp.jsx
@@ -1,42 +1,15 @@
-import { useState } from "react";
 import PropTypes from "prop-types";
 
-export const FirstApp = ({ title, subTitle }) => {
-  const estudiante = {
-    nombre: "Jefferson",
-    apellido: "Alquinga",
-    edad: 27,
-    direccion: "Puembo-chiche",
-    lugar: "Quito",
-  };
+export const FirstApp = ({ title, subTitle, name }) => {
 
-  const [esGraduado, setEsGraduado] = useState(null);
-
-  const estudianteFunction = (graduado) =>
-    graduado === true ? "Sí está graduado" : "No está graduado";
-
-  const handleGraduado = (respuesta) => {
-    setEsGraduado(respuesta === "SÍ");
-  };
 
   return (
     <>
-      <h1>
-        {estudiante.nombre} {estudiante.apellido}
-      </h1>
-      <h1>
-        {title} esto es un {subTitle}
-      </h1>
-      <code>{JSON.stringify(estudiante, null, 2)}</code>
-      <div>
-        <p>
-          ¿{estudiante.nombre} {estudiante.apellido} es graduado?
-        </p>
-        <button onClick={() => handleGraduado("SÍ")}>SÍ</button>
-        <button onClick={() => handleGraduado("NO")}>NO</button>
-      </div>
-      {esGraduado !== null && <h1>{estudianteFunction(esGraduado)}</h1>}
-      <p>Estudiante de Ingeniería</p>
+      <h1>{title}</h1>
+      <p>{subTitle}</p>
+      <p>{name}</p>
+      
+    
     </>
   );
 };
