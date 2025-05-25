afdc156dc1ed84894f2b3d6451c4db2636799fd2
Jeffers199817 <jefferson.alquinga1998@gmail.com>
2025-05-24 22:41:06 -0500

    Integración de test con react

--- 03-counter-app-vite/src/FirstApp.jsx
+++ 03-counter-app-vite/src/FirstApp.jsx
@@ -1,16 +1,20 @@
 import PropTypes from "prop-types";
 
-export const FirstApp = ({
-  title = "Hola, soy Vegeta"}) => {
+export const FirstApp = ({title = "Hola, soy Vegeta" , subtitulo="No hay subtitulo", name="Jefferson"}) => {
   return (
     <>
-      <h1>{title}</h1>
+      <h1 data-testid ='test-title'>{title} </h1>
+      <h1>{subtitulo}</h1>
+      <h1>{subtitulo}</h1>
+      <h1>{subtitulo}</h1>
+      <h1>{subtitulo}</h1>
+      <h1>{name}</h1>
       
     </>
   );
 };
 
 FirstApp.propTypes = {
-  title: PropTypes.string.isRequired,
+  //title: PropTypes.string.isRequired,
  
 };
--- 03-counter-app-vite/tests/FirstApp.test.jsx
+++ 03-counter-app-vite/tests/FirstApp.test.jsx
@@ -1,5 +1,6 @@
-import { render } from "@testing-library/react";
+import { getAllByText, render } from "@testing-library/react";
 import { FirstApp } from "../src/FirstApp";
+import { act } from "react";
 
 
 
@@ -9,9 +10,61 @@ describe("Pruebas en <FirstApp />", () => {
     test("Debe hacer match con el snapshot", () => {
       
         const title = "Hola, soy Vegeta";
+        let container;
+        act(() => {
+
+
+            ({ container } = render(<FirstApp title={title} />));
         
-     render(<FirstApp title={title}/>);
+        })      
+        expect(container).toMatchSnapshot();
     
 
-        });
+    });
+    
+
+    test('debe de mostar el tótulo en un h1', () => { 
+        const title = 'Hola, Johan';
+        const subtitulo = "NO existe numero";
+
+        const { container, getAllByText } = render(<FirstApp title={title} subtitulo={subtitulo} />);
+
+        expect(getAllByText(subtitulo)).toBeTruthy();
+
+
+        const h1 = container.querySelectorAll('h1');
+        console.log(h1)
+        console.log(h1[0].innerHTML);
+        console.log(h1[1].innerHTML);
+        console.log(h1[2].innerHTML);
+
+        expect(h1[0].innerHTML).toContain(title);
+    })
+
+    test("debe de mostar el tótulo en un h1", () => {
+        const title = "Hola, Johan";
+        const { container, getByText, getByTestId } = render(<FirstApp title={title} />)
+        expect(getByText(title)).toBeTruthy();
+
+
+        expect(getByTestId('test-title').innerHTML).toContain(title);
+
+
+        
+
+    });
+
+
+    test('debe de mostar el subtitulo enviado por props', () => { 
+
+
+        const subtitulo = "Este es el subtitulo";
+       // const title = 'este es el ttitulo';
+        const { getAllByText } = render(<FirstApp  subtitulo={subtitulo} />);
+        expect( getAllByText(subtitulo).length).toBe(4);
+    })
+    
+    
+
+
 });
