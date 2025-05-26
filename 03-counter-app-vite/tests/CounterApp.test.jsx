784b57ead8d5745948c658e45b6dc28fd07b2594
Jeffers199817 <jefferson.alquinga1998@gmail.com>
2025-05-25 20:38:58 -0500

    iNTEGRACIÓN DE EVENTOS

--- 03-counter-app-vite/tests/CounterApp.test.jsx
+++ 03-counter-app-vite/tests/CounterApp.test.jsx
@@ -7,18 +7,15 @@ describe('Pruebas en <CounterApp.jsx></CounterApp.jsx>', () => {
     const value = 100;
 
     test('debe de hacer match con el snapshot', () => {
-
-
-        const { container } = render(<CounterApp value={value} />)
-        
-        expect(container).toMatchSnapshot();
+    const { container } = render(<CounterApp value={value} />)
+    expect(container).toMatchSnapshot();
 
 
     })
 
-    test('Debe de mostrar el valor inicial de 100', () => { 
-
-        render(<CounterApp value={value} />)
-        expect(screen.getByRole('heading',{level:2}).innerHTML).toContain(`El valor es:${value}`);
+    test('Debe de mostrar el valor inicial de 100', () => {
+      render(<CounterApp value={value} />);
+        //expect(screen.getByRole('heading',{level:2}).innerHTML).toContain(`El valor es:${value}`);
+        expect(screen.getByText(value)).toBeTruthy();
     })
 })
